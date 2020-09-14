4dfa5798e94e39e242e3836a29f69d0895e90105
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2020-09-14 18:10:07 +0200

    Mise en place des fonctions stepElevator, enterElevator et exitElevator

---
+++ elevator.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include "elevator.h"
+
+
+Elevator* create_elevator(int capacity, int currentFloor, PersonList *persons){
+    Elevator* res = (Elevator*) malloc(sizeof(Elevator));
+    res->capacity = capacity;
+    res->currentFloor = currentFloor;
+    res->persons = persons;
+    res->targetFloor = persons->person->dest;
+    return res;
+}
+
+Building* create_building(int nbFloor, Elevator *elevator, PersonList **waitingLists){
+    Building* res = (Building*) malloc(sizeof(Building));
+    res->nbFloor = nbFloor;
+    res->elevator = elevator;
+    res->waitingLists = waitingLists;
+    return res;
+}
+
+PersonList* exitElevator(Elevator *e){
+    PersonList* res = (PersonList*) malloc(sizeof(PersonList));
+    res->next = NULL;
+    PersonList* personnes_dedans = e->persons;
+    while(personnes_dedans!=NULL){
+        if(personnes_dedans->person->dest==e->currentFloor){
+            res->person = personnes_dedans->person;
+            res->next = res;
+        }
+        personnes_dedans = personnes_dedans->next;
+    }
+    return res;
+
+}
+
+PersonList* enterElevator(Elevator *e, PersonList *waitingList){
+    PersonList* new_waitingList=waitingList;
+    PersonList* list_balayee = waitingList;
+    if(waitingList!=NULL){   
+        while((taille_PersonList(e->persons)<e->capacity) || (new_waitingList!=NULL)){
+            if(new_waitingList->person->src==e->currentFloor){
+                PersonList* new_cabine = insert(new_waitingList->person,e->persons);
+                e->persons=new_cabine;
+            }
+        
+            new_waitingList=new_waitingList->next;
+        }
+    }
+    return new_waitingList;
+}
+
+void stepElevator(Building *b){
+  if(b->elevator->currentFloor==b->elevator->targetFloor){
+    //des gens sortent de la cabine
+    b->elevator->persons=exitElevator(b->elevator);
+    b->elevator->targetFloor=b->elevator->persons->person->dest;
+    *b->waitingLists=enterElevator(b->elevator,*b->waitingLists);
+  }
+  else{
+      *b->waitingLists=enterElevator(b->elevator,*b->waitingLists);
+      if(b->elevator->currentFloor>b->elevator->targetFloor){
+          b->elevator->currentFloor--;
+      }
+        else{
+            b->elevator->currentFloor++;
+        }
+  }
+}
---
+++ person.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "person.h"
+
+Person* createPerson(int src, int dest){
+    Person* res = (Person*) malloc(sizeof(Person));
+    res->src = src;
+    res->dest = dest;
+    return res;
+}
+
+PersonList* insert(Person *p, PersonList *list){
+    PersonList* res = (PersonList*) malloc(sizeof(PersonList));
+    res->person = p;
+    res->next = list;
+    return res;
+}
+
+int taille_PersonList(PersonList *list){
+    int count = 0;
+    PersonList* copie=list;
+    while(copie != NULL){
+        count ++;
+        copie=copie->next;
+    }
+    return count;
+}
