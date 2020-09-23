00547ed70174c4563a59a0fb959d592529987f0d
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2020-09-23 23:38:10 +0200

    ajout de commentaires

--- person.c
+++ person.c
@@ -2,6 +2,7 @@
 #include "person.h"
 
 Person* createPerson(int src, int dest){
+    // Déclaration de la création d'une Person
     Person* res = (Person*) malloc(sizeof(Person));
     res->src = src;
     res->dest = dest;
@@ -9,15 +10,17 @@ Person* createPerson(int src, int dest){
 }
 
 PersonList* insert(Person *p, PersonList *list){
-    PersonList* res = (PersonList*) malloc(sizeof(PersonList));
-    res->person = p;
-    res->next = list;
+    // Fonction qui renvoie une PersonList à laquelle on a ajouté une Person
+    PersonList* res = (PersonList*) malloc(sizeof(PersonList)); // On alloue un nouvel espace à la PersonList
+    res->person = p; // on place la nouvelle Person en premier (arbitraire)
+    res->next = list; // on replace l'ancienne PersonList dans la nouvelle
     return res;
 }
 
 int taille_PL(PersonList *list){
+    // Fonction qui renvoie le nombre de Person contenues dans une PersonList
     int count = 0;
-    PersonList* copie=list;
+    PersonList* copie = list;
     while(copie != NULL){
         count ++;
         copie=copie->next;
--- person.h
+++ person.h
@@ -1,16 +1,19 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+// Définition de la structure de Person
 typedef struct _Person {
-    int src;
-    int dest;
+    int src; // numéro de l'étage d'où vient la personne
+    int dest; // numéro de l'étage où va la personne
 } Person;
 
+// Définition de la structure PersonList
 typedef struct _PersonList {
-    Person *person;
-    struct _PersonList *next;
+    Person *person; // première personne dans la liste
+    struct _PersonList *next; // liste des personnes restantes (peut être NULL)
 } PersonList;
 
+// Annonce des fonctions déclarées dans le fichier person.c 
 Person* createPerson(int src, int dest);
 PersonList* insert(Person *p, PersonList *list);
 int taille_PL(PersonList *list);
